80ff3ab0ac7f4c3b786ee2f6b7fa01a6f7293acc
Awwab <mawwabkhank2006@gmail.com>
2024-02-14 12:26:45 +0500

    length and email validators

--- login_form.py
+++ login_form.py
@@ -1,8 +1,8 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, PasswordField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, Length, Email
 
 class LoginForm(FlaskForm):
-    email = StringField(label='Email', validators=[DataRequired()])
-    password = PasswordField(label='Password', validators=[DataRequired()])
+    email = StringField(label='Email', validators=[DataRequired(), Email(check_deliverability=True)])
+    password = PasswordField(label='Password', validators=[DataRequired(), Length(8, 12)])
     submit = SubmitField(label='Go')
