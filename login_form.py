f0470e6d88c32cc0c4cbcbea6a06fcf6f49c557e
Awwab <mawwabkhank2006@gmail.com>
2024-02-14 12:09:11 +0500

    login form model

---
+++ login_form.py
@@ -0,0 +1,8 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
+
+class LoginForm(FlaskForm):
+    email = StringField('email', validators=[DataRequired()])
+    password = StringField('password', validators=[DataRequired()])
+    submit = SubmitField('go')
